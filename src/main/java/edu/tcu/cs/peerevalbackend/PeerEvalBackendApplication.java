e1b7cacad56b042688cae5af6f034bb148598fbc
hienkyle <hien2003.dm@gmail.com>
2024-03-21 19:53:10 -0500

    First commit.

---
+++ src/main/java/edu/tcu/cs/peerevalbackend/PeerEvalBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.peerevalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerEvalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerEvalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/tcu/cs/peerevalbackend/PeerEvalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.peerevalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerEvalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
