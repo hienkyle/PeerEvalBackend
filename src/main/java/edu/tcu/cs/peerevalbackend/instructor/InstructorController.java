cc951919a444206ca33cfab29bfcd362763a4d11
hienkyle <hien2003.dm@gmail.com>
2024-04-06 15:43:10 -0500

    remove method prototypes in Instructor

--- src/main/java/edu/tcu/cs/peerevalbackend/instructor/InstructorController.java
+++ src/main/java/edu/tcu/cs/peerevalbackend/instructor/InstructorController.java
@@ -2,6 +2,7 @@ package edu.tcu.cs.peerevalbackend.instructor;
 
 import edu.tcu.cs.peerevalbackend.system.Result;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,25 +15,4 @@ public class InstructorController {
         this.instructorService = instructorService;
     }
 
-//    public Result inviteInstructors(){
-//        return null;
-//    }
-
-    @GetMapping() // figure what to map this thing to
-    public Result findInstructors(){
-        return null;
-    }
-
-    @GetMapping()
-    public Result viewInstructor(){
-        return null;
-    }
-
-    public Result deactivateInstructor(){
-        return null;
-    }
-
-    public Result reactivateInstructor(){
-        return null;
-    }
 }
