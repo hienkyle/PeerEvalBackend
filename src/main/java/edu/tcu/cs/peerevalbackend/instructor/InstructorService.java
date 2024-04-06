e0cf9d19ddf70584ee25cdba86d757e0319c1494
hienkyle <hien2003.dm@gmail.com>
2024-04-06 15:28:23 -0500

    instructor crud method prototypes

---
+++ src/main/java/edu/tcu/cs/peerevalbackend/instructor/InstructorService.java
@@ -0,0 +1,14 @@
+package edu.tcu.cs.peerevalbackend.instructor;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class InstructorService {
+    InstructorRepository instructorRepository;
+
+    public InstructorService(InstructorRepository instructorRepository) {
+        this.instructorRepository = instructorRepository;
+    }
+}
