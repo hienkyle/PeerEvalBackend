ca409f91248287ff1eb7f86e377e0b6e4859fba8
Mason O'Connor <m.d.oconnor@tcu.edu>
2024-04-15 10:34:50 -0500

    initial war class

--- src/main/java/edu/tcu/cs/peerevalbackend/war/WeeklyActivityReport.java
+++ src/main/java/edu/tcu/cs/peerevalbackend/war/WeeklyActivityReport.java
@@ -1,4 +1,36 @@
 package edu.tcu.cs.peerevalbackend.war;
 
+import edu.tcu.cs.peerevalbackend.student.Student;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class WeeklyActivityReport {
+    @Id
+    Integer warId;
+
+    @ManyToOne
+    Student author;
+
+
+    String task;
+
+    String taskDesc;
+
+    String taskCategory;
+
+    String status;
+
+    String activeWeek;
+
+    Integer plannedHours;
+
+    Integer actualHours;
 }
+
